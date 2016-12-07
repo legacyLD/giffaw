d438a1ba925f156d50574d3c297e807323c84998
vedelopment <vedelopment@gmail.com>
2016-12-06 18:02:37 -0800

    adds cat search and displays images from giphy

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,24 @@
 $(document).on("ready", function(){
-
+  $('form').on("submit", searchReq);
+  function searchReq(giphyQ) {
+    giphyQ.preventDefault();
+    $.ajax ({
+      method:"GET",
+      url: "http://api.giphy.com/v1/gifs/search",
+      dataType: "json",
+      data:$('form').serialize(),
+      success: successCallback,
+      error: errorCallback,
+    });
+    function successCallback(responseData) {
+      console.log("success!");
+      var imageArray = responseData.data;
+      imageArray.forEach(function(object) {
+        $('.gif-gallery').append('<img src="' + object.images.fixed_height.url +'">');
+      });
+    };
+    function errorCallback() {
+      console.log("error!");
+    }
+  };
 });
